68aaff2ee45b938b796b6c69393a17d7d3e1b8ce
Rafael-Martins-Paiva <rafael.martins.paiva0@gmail.com>
2025-11-04 21:07:45 -0300

    update MensagemRepository.java

--- msn/src/main/java/dev/oldschool/msn/repository/MensagemRepository.java
+++ msn/src/main/java/dev/oldschool/msn/repository/MensagemRepository.java
@@ -4,5 +4,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import dev.oldschool.msn.model.Mensagem;
 
 public interface MensagemRepository extends JpaRepository<Mensagem Long> {
-  
 }
